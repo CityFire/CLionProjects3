a2b9e4cf093e06615488be613ad6bd7192b9dad1
CityFire <13787697167@163.com>
2023-03-18 01:36:47 +0800

    static用法和四种对象作用域与生存期

--- TestMain.cpp
+++ TestMain.cpp
@@ -206,10 +206,86 @@ public:
     int y_;
 };
 
+class Date
+{
+public:
+    Date(int year) : year_(year)
+    {
+
+    }
+    static bool isLeapYear(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+    bool isLeapYear()
+    {
+        return (year_ % 4 == 0 && year_ % 100 != 0) || (year_ % 400 == 0);
+    }
+private:
+    int year_;
+};
+
+class Test
+{
+public:
+    Test(int n) : n_(n)
+    {
+        cout<<"Test "<<n_<<"..."<<endl;
+    }
+    ~Test()
+    {
+        cout<<"~Test "<<n_<<"..."<<endl;
+    }
+private:
+    int n_;
+};
+
+int n; // 未初始化的全局变量，初始值为0。n存储于.bss段中。(Block Started by Symbol) 不占用空间
+int n2 = 100; // 已初始化的全局变量，初始值为100.n2存储于.data段中。
+Test g(100);    // 全局对象的构造先于main函数
+static Test g2(200);
+
+int main(void)
+{
+    cout<<"Entering main..."<<endl;
+    cout<<n<<endl;
+    Test t(10);      // 栈上创建的对象，在生存期结束的时候自动释放
+    {
+        Test t(20);
+    }
+
+    Test* t3 = new Test(30);   //堆上创建的对象，要显示释放
+    delete t3;
+
+    {
+        static int n3;       // n3存储于.bss段中  （编译期初始化）
+        static int n4 = 100; // n4存储于.data段中 （编译期初始化）
+
+        static Test t4(333); // t4对象运行期初始化  .data段中
+    }
+
+    cout<<"Exiting main..."<<endl;
+    return 0;
+}
+
+int main1320(void)
+{
+//    Date d(2012);
+//    cout<<d.isLeapYear()<<endl;
+
+      //非static成员
+//    Date d(2012);
+//    cout<<d.isLeapYear(2010)<<endl;
+
+    cout<<Date::isLeapYear(2010)<<endl;
+
+    return 0;
+}
+
 //const int TestDemo::x_ = 200;  // 静态成员的定义性说明
 int TestDemo::x_ = 100;  // 静态成员的定义性说明
 
-int main(void )
+int main1322(void )
 {
     TestDemo t(10);
     cout<<TestDemo::x_<<endl;
