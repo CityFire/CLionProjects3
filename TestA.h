b3cbe1950c58a1aac01364647dae0d65935c232f
CityFire <13787697167@163.com>
2023-03-17 16:59:33 +0800

    正常编译

--- TestA.h
+++ TestA.h
@@ -11,7 +11,7 @@ public:
     //如果类不提供任何一个构造函数，系统将为我们提供一个不带参数的
     //默认构造函数
     TestA();
-    explicit TestA(int num); //explicit 1.只提供给类的构造函数使用的关键字。2.编译器不会把声明为explicit的构造函数用于隐式转换，它只能在程序代码中显示创建对象
+    /*explicit*/ TestA(int num); //explicit 1.只提供给类的构造函数使用的关键字。2.编译器不会把声明为explicit的构造函数用于隐式转换，它只能在程序代码中显示创建对象
     void Display();
 
     TestA& operator=(const TestA& other);
