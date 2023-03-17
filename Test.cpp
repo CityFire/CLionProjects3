1ff94c21585741e0e9ce20acd277562cd14a4afe
CityFire <13787697167@163.com>
2023-03-17 16:03:46 +0800

    test:析构函数显式调用，但一般很少用

--- Test.cpp
+++ Test.cpp
@@ -70,39 +70,42 @@ void Fun()
     lc.Display();
 }
 
-TestA t(30); //全局对象先于main函数
+//TestA t(30); //全局对象先于main函数
 int main(void)
 {
     cout<<"Entering main..."<<endl;
-    Outer o;
-    o.Fun();
-    o.FunStruct();
-
-    Outer::Inner i; // note: implicitly declared private here
-    i.Func();
-
-    Fun();
+//    Outer o;
+//    o.Fun();
+//    o.FunStruct();
+//
+//    Outer::Inner i; // note: implicitly declared private here
+//    i.Func();
+//
+//    Fun();
     //LocalClass lc;   // Error,局部类只能在定义它的函数体中使用
 
-    TestA t;  // error: no matching constructor for initialization of 'TestA'
-    t.Display();
+//    TestA t;  // error: no matching constructor for initialization of 'TestA'
+//    t.Display();
+//
+//    TestA t2(10);
+//    t2.Display();
 
-    TestA t2(10);
-    t2.Display();
+//    TestA* t3 = new TestA(20); // new operation
+//    t3->Display();
+//
+//    delete t3;
+//
+//    TestA t4[2] = {100, 200};
+//
+//    TestA* t5 = new TestA(300);
+//    delete t5;
+//
+//    TestA* t6 = new TestA[2]; //数组
+//    //delete t6; //Demo(43373,0x1185abe00) malloc: *** error for object 0x7fcbe0c058d8: pointer being freed was not allocated
+//    delete[] t6;
 
-    TestA* t3 = new TestA(20); // new operation
-    t3->Display();
-
-    delete t3;
-
-    TestA t4[2] = {100, 200};
-
-    TestA* t5 = new TestA(300);
-    delete t5;
-
-    TestA* t6 = new TestA[2]; //数组
-    //delete t6; //Demo(43373,0x1185abe00) malloc: *** error for object 0x7fcbe0c058d8: pointer being freed was not allocated
-    delete[] t6;
+    TestA t7;
+    t7.~TestA(); //析构函数可以显式调用，但一般很少用。
 
     cout<<"Exiting main..."<<endl;
 
