6f7d2dcba8357ba80c2758d80f28ab354dad96f5
CityFire <13787697167@163.com>
2023-03-19 22:26:48 +0800

    析构函数 内存泄露

--- VirtualFunTestMain.cpp
+++ VirtualFunTestMain.cpp
@@ -21,6 +21,16 @@ public:
     {
         cout<<"VirtualBase::Fun3..."<<endl;
     }
+
+    VirtualBase()
+    {
+        cout<<"VirtualBase..."<<endl;
+    }
+
+    ~VirtualBase()
+    {
+        cout<<"~VirtualBase"<<endl;
+    }
 };
 
 class VirtualDerived : public VirtualBase
@@ -40,17 +50,34 @@ public:
     {
         cout<<"VirtualDerived::Fun3..."<<endl;
     }
+
+    VirtualDerived()
+    {
+        cout<<"VirtualDerived..."<<endl;
+    }
+
+    ~VirtualDerived()
+    {
+        cout<<"~VirtualDerived"<<endl;
+    }
 };
 
 int main(void)
 {
-    VirtualBase* p;
-    VirtualDerived d;
+//    VirtualBase* p;
+//    VirtualDerived d;
+//
+//    p = &d;
+//    p->Fun1();   // Fun1是虚函数，基类p指针指向派生类对象，调用的是派生类对象的虚函数
+//    p->Fun2();
+//    p->Fun3();   // Fun3非虚函数，根据p指针实际类型来调用相应类的成员函数
 
-    p = &d;
-    p->Fun1();   // Fun1是虚函数，基类p指针指向派生类对象，调用的是派生类对象的虚函数
-    p->Fun2();
-    p->Fun3();   // Fun3非虚函数，根据p指针实际类型来调用相应类的成员函数
+
+    VirtualBase* p;
+    p = new VirtualDerived;
+
+    p->Fun1();
+    delete p;
 
     return 0;
 }
