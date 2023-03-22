6e8277c68fd490f21bd49f188857ff08fdb4285e
CityFire <13787697167@163.com>
2023-03-22 22:09:32 +0800

    禁止拷贝 问题解析

--- Demo/SingletonAutoPtrTestMain.cpp
+++ Demo/SingletonAutoPtrTestMain.cpp
@@ -5,6 +5,51 @@
 #include <iostream>
 using namespace std;
 
+class Noncopyable
+{
+protected:
+    Noncopyable() {}
+    ~Noncopyable() {}
+private:
+    Noncopyable(const Noncopyable&);
+    const Noncopyable& operator=(const Noncopyable&);
+};
+
+class Parent : private  Noncopyable
+{
+public:
+    Parent() : Noncopyable()
+    {
+        cout<<"Parent()..."<<endl;
+    }
+    Parent(const Parent& other)// : Noncopyable(other)   // 拷贝构造函数不规范引起 使用默认拷贝构造函数也会调用基类的拷贝构造函数
+    // 不写上: Noncopyable(other) 这个 是不会调用基类拷贝构造函数的
+    {
+       cout<<"Parent(const Parent& other)..."<<endl;
+    }
+    ~Parent()
+    {
+        cout<<"~Parent()..."<<endl;
+    }
+};
+
+class Child : public  Parent
+{
+public:
+    Child() : Parent()
+    {
+        cout<<"Child()..."<<endl;
+    }
+    Child(const Child& other) : Parent(other)
+    {
+       cout<<"Child(const Child& other)..."<<endl;
+    }
+    ~Child()
+    {
+        cout<<"~Child()..."<<endl;
+    }
+};
+
 class SingletonAutoPtr
 {
 public:
@@ -56,6 +101,14 @@ int main(void)
 //    SingletonAutoPtr s3 = *s1;
 
 //    SingletonAutoPtr::Free();
+
+
+    //////////////
+//    Parent p1;
+//    Parent p2(p1);  // 要调用Parent拷贝构造函数，Parent构造函数又调用Noncopyable的拷贝构造函数
+
+    Child c1;
+    Child c2(c1);
 
     return 0;
 }
