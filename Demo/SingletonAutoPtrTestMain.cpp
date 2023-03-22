b34fed0d47a30f9e896e2cfa60dacdad9af5e86f
CityFire <13787697167@163.com>
2023-03-22 23:47:20 +0800

    3个#define宏

--- Demo/SingletonAutoPtrTestMain.cpp
+++ Demo/SingletonAutoPtrTestMain.cpp
@@ -5,6 +5,49 @@
 #include <iostream>
 using namespace std;
 
+// 两个指针相减，得到的是相隔几个元素
+#define sizeof_v(x) (char*)(&x+1) - (char*)(&x)
+
+#define sizeof_t(t) ((size_t)((t*)0 + 1))
+// 对齐
+#define ALIGN(v, b) ((v+b-1) & ~(b-1))
+
+/*
+   0011
+   1111
+  10000
+   0000
+
+  10000 = 16
+
+  原理、思想
+
+  某个数要对齐到16的整数倍    0000     32的整数倍00000
+  1000
+
+  向上对齐  3 + 15
+  超出的部分我们要把它抹除掉（低4倍都置0）
+
+  0
+  ALIGN(0, 16) = 0
+  ALIGN(16,16) =
+
+  10000
+   1111
+  11111
+
+  内存池
+  3 7 9
+  内存池中 内存块大小是规则的
+  产生空隙 碎片
+
+  */
+
+class Empty
+{
+
+};
+
 class Noncopyable
 {
 protected:
@@ -109,6 +152,25 @@ int main(void)
 
     Child c1;
     Child c2(c1);
+
+
+
+    /////////////
+    Empty e;
+    int n;
+//    cout<<sizeof(e)<<endl;       // 1
+//    cout<<sizeof(Empty)<<endl;   // 1
+
+    cout<<sizeof_v(e)<<endl;
+    cout<<sizeof_v(n)<<endl;
+    cout<<sizeof_t(Empty)<<endl;
+    cout<<sizeof_t(int)<<endl;
+
+
+    cout<<ALIGN(3, 16)<<endl;
+    cout<<ALIGN(31, 16)<<endl;
+    cout<<ALIGN(0, 16)<<endl;
+    cout<<ALIGN(4198, 4096)<<endl;
 
     return 0;
 }
