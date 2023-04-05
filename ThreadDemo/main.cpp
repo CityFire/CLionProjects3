eaea4d27897d51ab2504cb9ba70b65634625feb4
CityFire <13787697167@163.com>
2023-04-05 13:40:02 +0800

    条件变量改造转账系统

--- ThreadDemo/main.cpp
+++ ThreadDemo/main.cpp
@@ -490,8 +490,19 @@ public:
 public:
     void changeMoney(double amount)
     {
+        unique_lock lock(mMoneyLock); // 这里使用的是unique_lock，这是为了与条件变量相配合。因为条件变量会解锁和重新锁定互斥体。
+        mConditionVar.wait(lock, [this, amount] { // 这里是比较重要的一个地方：通过条件变量进行等待。
+            // 此时：会通过后面的lambda表达式判断条件是否满足。如果满足则继续；如果不满足，则此处会解锁互斥
+            // 体，并让当前线程等待。解锁这一点非常重要，因为只有这样，才能让其他线程获取互斥体。
+            return mMoney + amount > 0; // 这里是条件变量等待的条件。
+        });
         mMoney += amount;
+        mConditionVar.notify_all(); // 此处也很重要。当金额发生变动之后，我们需要通知所有在条件变量上等待
+        // 的其他线程。此时所有调用wait线程都会再次唤醒，然后尝试获取锁（当然，只有一个能获取到）并再次判断条件
+        // 是否满足。除了notify_all还有notify_one，它只通知一个等待的线程。wait和notify就构成了线程间互相协作的工具。
     }
+    // 请注意：wait和notify_all虽然是写在一个函数中的，但是在运行时它们是在多线程环境中执行的，因此对于这段代码，需要能够
+    // 从不同线程的角度去思考代码的逻辑。这也是开发并发系统比较难的地方。
     string getName() {
         return mName;
     }
@@ -506,6 +517,7 @@ private:
     string mName;
     double mMoney;
     mutex mMoneyLock;
+    condition_variable mConditionVar; // 声明了一个条件变量，用来在多个线程之间协作
 };
 
 class Bank
@@ -515,7 +527,7 @@ public:
         mAccounts.insert(account);
     }
 
-    bool transferMoney(Account* accountA, Account* accountB, double amount) {
+    bool transferMoney_lock_guard(Account* accountA, Account* accountB, double amount) {
         lock(*accountA->getLock(), *accountB->getLock()); // 通过lock函数来获取两把锁，标准库的实现会保证不会发生死锁。
         lock_guard guardA(*accountA->getLock(), adopt_lock); // 在自身对象生命周期的范围内锁定互斥体
         lock_guard guardB(*accountB->getLock(), adopt_lock); // 创建lock_guard的目的是为了在transferMoney结束的时候释放锁，
@@ -561,16 +573,37 @@ public:
         }
         return sum;
     }
+    void transferMoney(Account* accountA, Account* accountB, double amount);
 private:
     set<Account*> mAccounts;
 };
+
+void Bank::transferMoney(Account *accountA, Account *accountB, double amount) {
+    accountA->changeMoney(-amount);
+    accountB->changeMoney(amount);
+}
 
 mutex sCoutLock;
 void randomTransfer(Bank* bank, Account* accountA, Account* accountB)
 {
     while (true) {
         double randomMoney = ((double)rand() / RAND_MAX) * 100;
-        if (bank->transferMoney(accountA, accountB, randomMoney)) {
+        {
+            lock_guard guard(sCoutLock);
+            cout << "Try to Transfer " << randomMoney
+                 << " from " << accountA->getName() << "(" << accountA->getMoney()
+                 << ") to " << accountB->getName() << "(" << accountB->getMoney()
+                 << "), Bank totalMoney: " << bank->totalMoney() << endl;
+        }
+        bank->transferMoney(accountA, accountB, randomMoney);
+    }
+}
+
+void randomTransfer_lock_guard(Bank* bank, Account* accountA, Account* accountB)
+{
+    while (true) {
+        double randomMoney = ((double)rand() / RAND_MAX) * 100;
+        if (bank->transferMoney_lock_guard(accountA, accountB, randomMoney)) {
             sCoutLock.lock();
             cout << "Transfer " << randomMoney << " from " << accountA->getName()
                  << " to " << accountB->getName()
@@ -590,7 +623,7 @@ void randomTransferMaybeDeadLock(Bank* bank, Account* accountA, Account* account
 {
     while (true) {
         double randomMoney = ((double)rand() / RAND_MAX) * 100;
-        if (bank->transferMoney(accountA, accountB, randomMoney)) {
+        if (bank->transferMoney_lock_guard(accountA, accountB, randomMoney)) {
             cout << "Transfer " << randomMoney << " from " << accountA->getName()
                  << " to " << accountB->getName()
                  << ", Bank totalMoney: " << bank->totalMoney() << endl;
@@ -675,7 +708,7 @@ RAII 可总结如下:
     生产者和消费者共享一个工作区。这个区间的大小是有限的。
     生产者总是产生数据放入工作区中，当工作区满了。它就停下来等消费者消费一部分数据，然后继续工作。
     消费者总是从工作区中拿出数据使用。当工作区中的数据全部被消费空了之后，它也会停下来等待生产者往工作区中放入新的数据。
-    
+
 从上面可以看到，无论是生产者还是消费者，当它们工作的条件不满足时，它们并不是直接报错返回，而是停下来等待，直到条件满足。
  */
 
